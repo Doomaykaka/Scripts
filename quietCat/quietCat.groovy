949adf372f4d2cc8dfbb0c07cb9c0d1f9e6a17be
Doomaykaka <domaykaka@gmail.com>
2024-02-14 08:04:28 +0400

    Update

---
+++ quietCat/quietCat.groovy
@@ -0,0 +1,496 @@
+import java.util.logging.Level
+import java.util.logging.Logger
+import java.util.zip.ZipOutputStream
+import java.util.zip.ZipEntry
+import groovy.cli.commons.CliBuilder
+import java.nio.file.Path
+import java.nio.file.Paths
+import groovy.io.FileType
+import java.nio.file.*
+import java.nio.file.attribute.BasicFileAttributes
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import groovy.json.*
+
+class Constants {
+
+    def static final EXCLUSION_FILENAMES = ['quietCat.groovy', 'Templates']
+
+}
+
+class Template {
+
+    Insertations insertations
+    List<TemplateFolder> templateFolders
+    List<TemplateFile> templateFiles
+    String name
+    String description
+    Path currentPath = Paths.get("./")
+
+    Template (String name, String description) {
+
+        this.insertations = new Insertations()
+        this.templateFolders = []
+        this.templateFiles = []
+        this.name = name
+        this.description = description
+
+    }
+
+    def parse() {
+
+        File currentFolder = new File(currentPath.toString())
+
+        for (File folderElement : currentFolder.listFiles()) {
+            if (!Constants.EXCLUSION_FILENAMES.contains(folderElement.getName())) {
+                if (folderElement.isDirectory()) {
+                    TemplateFolder folder = new TemplateFolder(currentPath, folderElement.getName())
+                    folder.parse()
+                    templateFolders.add(folder)
+                } else {
+                    TemplateFile file = new TemplateFile(currentPath, folderElement.getName())
+                    file.parse()
+                    templateFiles.add(file)
+                }
+            }
+        }
+
+    }
+
+    def generate(){
+
+        for (TemplateFolder folder : templateFolders) {
+            folder.generate(insertions)
+        }
+
+        for (TemplateFile file : templateFiles) {
+            file.generate(insertions)
+        }
+    }
+
+    def save(Path pathToTemplateFile){
+        def insertationsData = [:]
+
+        for (entry in (this.insertations).getInsertationsMap()) {
+            insertationsData.put(
+                                    [
+                                        "definition": entry.key.getDifinition(),
+                                        "type": entry.key.getType(),
+                                        "description": entry.key.getDescription(),
+                                    ] ,
+                                    entry.value
+                                )
+        }
+
+        def templateFoldersData = []
+
+        for (templateFolder in this.templateFolders){
+            templateFoldersData.add(templateFolder.save())
+        }
+
+        def templateFilesData = []
+
+        for (templateFile in this.templateFiles){
+            templateFoldersData.add(templateFile.save())
+        }
+
+        def templateData = [
+                                "insertions": insertationsData,
+                                "templateFolders": templateFoldersData,
+                                "templateFiles": templateFilesData,
+                                "name": this.name,
+                                "description": this.description,
+                            ]
+
+        def json = new JsonBuilder(templateData).toPrettyString()
+
+        println("<<<<<<<<<<<<" + json + ">>>>>>>>>>>>")
+
+        File saveFile = new File(pathToTemplateFile.toString())
+
+        FileWriter fw = new FileWriter(saveFile)
+        fw.write(json)
+        fw.close()
+    }
+
+    def load(Path pathToTemplateFile){
+
+    }
+
+}
+
+class TemplateFolder{
+
+    List<TemplateFolder> templateFolders
+    List<TemplateFile> templateFiles
+    String nameWithInsertation
+    Path currentPath
+
+    def TemplateFolder(Path parentDir, String nameWithInsertation){
+        templateFolders = []
+        templateFiles = []
+        this.nameWithInsertation = nameWithInsertation
+        currentPath = Paths.get(parentDir.toString(), nameWithInsertation)
+    }
+
+    void parse(){
+        File currentFolder = new File(currentPath.toString())
+
+        for(File folderElement : currentFolder.listFiles()){
+            if(!Constants.EXCLUSION_FILENAMES.contains(folderElement.getName())){
+                if(folderElement.isDirectory()){
+                    TemplateFolder folder = new TemplateFolder(currentPath, folderElement.getName())
+                    folder.parse()
+                    templateFolders.add(folder)
+                } else {
+                    TemplateFile file = new TemplateFile(currentPath, folderElement.getName())
+                    file.parse()
+                    templateFiles.add(file)
+                }
+            }
+        }
+    }
+
+    void generate(Insertations insertions) {
+        File folder = new File(Paths.get(currentPath.parent, insertations.replace(nameWithInsertation)))
+
+        folder.mkdirs()
+    }
+
+    List save() {
+        def saveStruct = []
+
+        def templateFoldersStruct = []
+
+        for (raw : templateFolders) {
+            templateFoldersStruct.add(raw.save())
+        }
+
+        def templateFilesStruct = []
+
+        for (raw : templateFiles) {
+            templateFilesStruct.add(raw.save())
+        }
+
+        saveStruct.add(
+            [
+                'templateFolders' : templateFoldersStruct,
+                'templateFiles' : templateFilesStruct,
+                'nameWithInsertation' : nameWithInsertation,
+                'currentPath' : currentPath.toString(),
+            ]
+        )
+
+        return saveStruct
+    }
+
+    def load(){
+        return
+    }
+
+}
+
+class TemplateFile {
+
+    List<String> content
+    String nameWithInsertation
+    Path currentPath
+
+    TemplateFile(Path parentDir, String nameWithInsertation) {
+        content = []
+        this.nameWithInsertation = nameWithInsertation
+        currentPath = Paths.get(parentDir.toString(), nameWithInsertation)
+    }
+
+    void parse(Insertations insertions) {
+        println(currentPath)
+        File currentFile = new File(currentPath.toString())
+
+        Scanner scn = new Scanner(currentFile)
+        while (scn.hasNextLine()) {
+            content.add(scn.nextLine())
+        }
+        scn.close()
+    }
+
+    void generate(Insertations insertions) {
+        File file = new File(Paths.get(currentPath.parent, insertations.replace(nameWithInsertation)))
+
+        if (file.createNewFile()) {
+            FileWriter fw = new FileWriter(file)
+
+            for(String raw : content){
+                fw.write(insertations.replace(raw))
+            }
+
+            fw.close()
+        }
+    }
+
+    List save() {
+        def saveStruct = []
+
+        def contentStruct = []
+
+        for (raw : content) {
+            contentStruct.add(raw)
+        }
+
+        saveStruct.add(
+            [
+                'content' : contentStruct,
+                'nameWithInsertation' : nameWithInsertation,
+                'currentPath' : currentPath.toString(),
+            ]
+        )
+
+        return saveStruct
+    }
+
+    def load() {
+        return
+    }
+
+}
+
+class Insertations {
+
+    Map<Insertation, String> insertationsMap
+
+    Insertations() {
+        insertationsMap = [:]
+    }
+
+    Boolean put(Insertation insertation, String value) {
+        if (insertationsMap.containsKey(insertation)) {
+            if (validateInsertationValue(insertation, value)) {
+                insertationsMap.put(insertation, value)
+            }
+        }
+    }
+
+    void parse(String text){
+        matches = (text =~ /\[\\*\/\]/)
+
+        for (match : matches) {
+            Insertation insertation = new Insertation(match, match[2..-2])
+            put(insertation, null)
+        }
+    }
+
+    String replace(String text){
+        String result = text
+
+        for (Insertation insertation:insertationsMap.keys) {
+            result.replace(insertation.type , insertationsMap.get(insertation))
+        }
+
+        return result
+    }
+
+    void clear() {
+        insertationsMap.clear()
+    }
+
+    private Boolean validateInsertationValue(Insertation insertation, String value) {
+        String typeRegexp = insertation.type
+
+        return value.matches(typeRegexp)
+    }
+
+}
+
+class Insertation {
+
+    String definition
+    String type
+    String description
+
+    Insertation(String definition, String type, String description){
+        this.definition = definition
+        this.type = type
+        this.description = description
+    }
+
+}
+
+def app() {
+    printAppOutput(text="Hello", is_title=false, level=0, prefix="", postfix="", needNewLine=true)
+    printAppOutput(text="QuietCat", is_title=true, level=0, prefix="", postfix="", needNewLine=true)
+
+    createCLI()
+
+    printAppOutput(text="Done!", is_title=false, level=0, prefix="", postfix="", needNewLine=true)
+}
+
+def printAppOutput(String text, Boolean is_title, Integer level, String prefix, String postfix, Boolean needNewLine) {
+    outputText = text
+
+    if(is_title){
+        outputText = "//----------" + text + "----------\\\\"
+        if(needNewLine){
+            println(outputText)
+        } else {
+            print(outputText)
+        }
+
+        return
+    }
+
+    if(prefix) {
+        outputText = prefix + outputText
+    }
+
+    if(postfix) {
+        outputText = outputText + postfix
+    }
+
+    if(level > 0) {
+        outputText = " " * level + outputText
+    }
+
+    if(needNewLine){
+        println(outputText)
+    } else {
+        print(outputText)
+    }
+}
+
+def createCLI(){
+    cliAnswer = createCLIQuestion(
+                    questionText = "You need create or load template? (0 - create, 1 - load)",
+                    isAppQuestion = true,
+                ) as Integer
+
+    templatesPath = downloadTemplates()
+
+    switch(cliAnswer){
+        case 0:
+            createTemplateCLI(templatesPath)
+            break
+        case 1:
+            loadTemplateCLI(templatesPath)
+            break
+        default:
+            createCLI()
+            printAppOutput(text="Bad answer", is_title=false, level=0, prefix="!", postfix="", needNewLine=true)
+            break
+    }
+
+    printAppOutput(text="Clearing", is_title=false, level=0, prefix="", postfix="", needNewLine=true)
+
+    clearTemplatesFolder()
+}
+
+def Path downloadTemplates(){
+    gitRepo = createCLIQuestion(questionText = "Input git repo with templates link (url)", isAppQuestion = true)
+
+    executeCommands(
+        [
+            "mkdir",
+            "Templates",
+        ].join(" "),
+        [
+            "cd",
+            "Templates",
+        ].join(" "),
+        [
+            "git",
+            "clone",
+            gitRepo,
+        ].join(" "),
+    )
+
+    repoFolderNameMatches = (gitRepo =~ /\/(.+)\.git/)
+    repoFolderNameFirstMatch = repoFolderNameMatches[0]
+    repoFolderNameFirstFullMatch = repoFolderNameFirstMatch[1]
+
+    Path pathToTemplates = Paths.get(".", "Templates", repoFolderNameFirstFullMatch)
+
+    return pathToTemplates
+}
+
+def createTemplateCLI(Path pathToTemplatesFolder){
+    templateName = createCLIQuestion(
+                                        questionText = "Input template name (example: SimpleApplication)",
+                                        isAppQuestion = false
+                                    )
+    templateDescription = createCLIQuestion(
+                                                questionText =
+                                                "Input template description (example: simple application)",
+                                                isAppQuestion = false
+                                            )
+
+    Template newTemplate = new Template(name = templateName, description = templateDescription)
+    newTemplate.parse()
+
+    newTemplate.save(Paths.get(pathToTemplatesFolder.toString(), templateName + ".json"))
+    uploadTemplates()
+}
+
+def loadTemplateCLI(Path pathToTemplatesFolder){
+    // templateNumber = createCLIQuestion(
+    //                                     questionText = "Input template name (example: SimpleApplication)",
+    //                                     isAppQuestion = false
+    //                                 )
+}
+
+def clearTemplatesFolder(){
+    executeCommand(
+        [
+            "rm",
+            "-r",
+            "./Templates",
+        ].join(" ")
+    )
+}
+
+def String createCLIQuestion(String questionText, Boolean isAppQuestion){
+    if(isAppQuestion){
+        printAppOutput(text=questionText, is_title=false, level=1, prefix="", postfix=": ", needNewLine=false)
+    } else {
+        printAppOutput(text=questionText, is_title=false, level=2, prefix="?[", postfix="]: ", needNewLine=false)
+    }
+
+    return System.in.newReader().readLine()
+}
+
+def executeCommands(String[] commands){
+    unionCommand = ""
+
+    for(command : commands){
+        unionCommand += command + ";"
+    }
+
+    executeCommand(unionCommand)
+}
+
+def executeCommand(String command){
+    Path currentDir = Paths.get((new File(".")).getAbsolutePath())
+
+    def commandWrapper = [
+        'cd',
+        """\"${currentDir.toString()}\"""",
+        ';',
+        command,
+    ].join(' ')
+
+    def executer = [
+        'bash',
+        '-c',
+        '\'',
+        commandWrapper.toString(),
+        '\'',
+    ].join(' ')
+
+    Process process = executer.execute()
+
+    new StringWriter().with {
+         o -> new StringWriter().with {
+            e -> process.waitForProcessOutput(o, e)
+            [ o, e ]*.toString()
+         }
+    }
+}
+
+app()
